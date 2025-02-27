0973f776d135ab485537940332931883886f7f1a
Lars Jensen <laje63255@edu.ucl.dk>
2025-02-27 13:44:43 +0100

    Fejl

--- VejrApp/Controllers/WeatherController.cs
+++ VejrApp/Controllers/WeatherController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VejrApp.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace VejrApp.Controllers
 {
     public class WeatherController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(double lon, double lat)
         {
             APIHandler Client = new();
-            return View();
+
+            try
+            {
+                var Response = Client.Get(lon, lat);
+
+                Response.EnsureSuccessStatusCode();
+
+                string JsonData = Response.Content.ReadAsString();
+
+                WeatherMeasurement measurement = JsonConvert.DeserializeObject<WeatherMeasurement>(JsonData);
+
+                return View(measurement);
+
+            }
+            
+            catch (HttpRequestException ex)
+            {
+
+            }
         }
     }
 }
--- VejrApp/Models/WeatherMeasurement.cs
+++ VejrApp/Models/WeatherMeasurement.cs
@@ -2,20 +2,53 @@
 {
     public class WeatherMeasurement
     {
-        public double Lon {  get; set; }
-        public double Lat { get; set; }
-        public double Temp {  get; set; }
-        public double FeelsLike { get; set; }
-        public double TempMin { get; set; }
-        public double TempMax { get; set; }
-        public double Pressure { get; set; }
-        public double Humidity { get; set; }
-        public int Visibility { get; set; }
-        public double WindSpeed { get; set; }
-        public double WindDeg { get; set; }
-        public double WindGust { get; set; }
-        public double Cloudyness { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
+        public Coord Coord { get; set; }
+        public Weather[] Weather { get; set; }
+        public Main Main { get; set; }
+        public Wind Wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public Sys Sys { get; set; }
+        public string Name { get; set; }
     }
+
+    public class Coord
+    {
+        public float Lon { get; set; }
+        public float Lat { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class Main
+    {
+        public float Temp { get; set; }
+        public float Feels_Like { get; set; }
+        public int Humidity { get; set; }
+        public int Pressure { get; set; }
+    }
+
+    public class Wind
+    {
+        public float Speed { get; set; }
+        public int Deg { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+
+    public class Sys
+    {
+        public string Country { get; set; }
+        public long Sunrise { get; set; }
+        public long Sunset { get; set; }
+    }
+
 }
