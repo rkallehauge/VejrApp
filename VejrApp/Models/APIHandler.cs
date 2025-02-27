68df072adca2c27a02dd6bf512e6b20e504d543a
Mathilde <you@example.com>
2025-02-27 11:29:35 +0100

    APIController

---
+++ VejrApp/Models/APIHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VejrApp.Models
+{
+   
+    public class APIHandler
+    {
+        HttpClient Client;                          //Deklarer variable der hedder Client
+        public APIHandler()                                 //Construktor
+        {
+            Client = new HttpClient();               //Intialiserer variablen
+            
+            Uri API = new Uri("https://api.openweathermap.org/data/2.5/weather");   //vi gemmer url´en
+            Client.BaseAddress = API;                                               //Når der kommer forspørgelser, så peger den her hen
+        }
+
+        public async HttpResponseMessage Get(double lon, double lat)            //
+        {
+            string apiKey = Models.StaticSecret.Secret;
+        }
+            
+    }
+
+
+}
