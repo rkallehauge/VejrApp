1964ef1fa276dde419db5c16d656003ded75a27b
Rasmus Wissing Kallehauge <rkallehauge@gmail.com>
2025-02-27 09:54:36 +0100

    init commit

---
+++ VejrApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VejrApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
